750121014fb7b24832acea7bb4579b38d14e6e2d
Admin <Admin@Bedroom>
2023-11-05 11:35:59 -0800

    Update to chromedriver-2, fixed typo

--- src/thebrowserbots/com/AboutBox.java
+++ src/thebrowserbots/com/AboutBox.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package peteythepervert.com;
+package thebrowserbots.com;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
